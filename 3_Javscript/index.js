afb255f94883b199a3befc013ea7964a5b34a528
razaullahchitrali <rrizvi340@gmail.com>
2024-02-15 23:16:49 +0500

    f:Basics of Javascript

---
+++ 3_Javscript/index.js
@@ -0,0 +1,98 @@
+// var per = 15;
+// var age =5;
+
+// If else if conditions//
+// var per =  prompt("Enter your percentage.");
+
+// if (per >= 80 && per <= 99) { document.write("you are in merit"); }
+// else if (per > 60 && per < 79) { document.write("you are in 1st position"); } else if (per > 40 && per < 60) { document.write("you are in 2nd position"); }
+// else if (per > 25 && per < 40) { document.write("you are in 3rd position"); }else if (per > 10 && per < 25) { document.write("you are in Fail catagary"); }
+// switch (true) {
+//             //   Switch statement in javascript  
+//     case  ( age >= 10 && age <= 12):
+//         document.write("You are Acceptible");
+//         break;
+//     case (age >= 15) && (age <= 20):
+//         document.write("You are in progress");
+
+//         break;
+//     case  (age >= 20 && age <= 35):
+//         document.write("You are not Acceptible");
+//         break;
+
+//     default: document.write("confirm your age and then come to the office");
+//         break;
+// }
+
+//             // /***Promote Box    */
+// var a = prompt("What is your occupation?");
+// alert(a);
+//**** Functions  syntamix in js */
+// function razaBhai() {
+//     document.write("Hello Raza bhai kese ho kya ha wa ahwal chal ra");
+// }
+// function yahooBhai(){ document.write("This is the way,that you");}
+// razaBhai();
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+// razaBhai();
+// razaBhai();
+// document.write("<br>");
+// yahooBhai();
+
+// // ** Confirm Box **///
+// var a = confirm(
+//     "Do you like our website"
+// )
+// if (a) {
+//     alert("Thanks");
+// }else{
+//     alert("Soory");
+// }
+
+////**** Functions */
+// function fullName(fname,lname) {
+//     document.write("Hello sir,Are you like our website");
+//     document.write("<br>");
+//     document.write("<br>");
+//     document.write("Hello sir,Are you like our website");
+//     document.write("<br>");
+//     document.write("<br>");
+//     document.write("Hello sir,Are you like our website");
+//     document.write("<br>");
+//     document.write("<br>");
+//     document.write("Hello sir,Are you like our website");
+// }
+// myName();
+// myName();
+// myName();
+// myName();
+// myName();
+// myName();
+
+// //** Function with Perameters */
+//  function fullName(fname,lname) {
+//     document.write('' + fname + lname);
+//     document.write("<br>");
+//     document.write('Helo, ' + fname + lname);
+//     document.write("<br>");
+//     document.write('Helo, ' + fname + lname);
+//     document.write("<br>");
+//     document.write('Helo, ' + fname + lname);
+//  }
+//  fullName (  'Raza  ' , 'Mateen');
+
+//  *****   Function with Return  ****///
+
+// function fullName(fname, lname) {
+//     var a = ("Hello ," + fname + lname);
+//     return a;
+// }
+// var b = fullName(" Gul ", " shahzeb")
+// document.write(b);
+                //  /*** Basic Events of Java script ***/
+               function hello(){
+                document.write("Hello every one")
+               }
+            //    hello();
