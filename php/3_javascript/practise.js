20744b9ac50055c648afdb2bcad5273fae9dda16
razaullahchitrali <rrizvi340@gmail.com>
2024-02-16 22:15:29 +0500

    f: Basics of javascript

---
+++ php/3_javascript/practise.js
@@ -0,0 +1,91 @@
+// loop is used to perform repeated task based on given condition (true of false)
+
+/**
+ * for(initialization, condition, increment){
+ *    // tasks
+ * }
+ * 
+ */
+// let users= ['Ali', 'Akram',"Ahmad"];
+
+// console.log("For loop start",users.length);
+
+// for(var i = 0; i<users.length; i ++){
+//    console.log(users[i]);
+
+// }
+//  *** Array ***/
+//  let subject = ['Maths ','Physics','Chemistry','bio','English ','urdu ','Islamiyat ','Social Media'];
+//  subject[6] = 'mathematics';           **for changing elements**
+// subject.splice(1,2);                   **removing element from center**
+//  subject.push('permenent');            ** For adding element at last**
+// let position = subject.indexOf('Chemistry');///***for finding a position***//
+//  console.log(position);
+// console.log(subjet.lenght);           *** Exicuting  the lenght" 
+//    subject.unshift();                 **add in starting**
+// subject.pop();                        ***remove the element from last**
+// subject.shift();                      ***remove from start***
+
+//                                   ** split convert text into Array   **//
+// let text = 'My name is Raza ullah i belong to chitral'
+// let textArray = text.split(' ');
+// console.log(textArray );
+//                                     **  for joining texts***///                 
+// let subject = ['Maths ','Physics','Chemistry','bio','English ','urdu ','Islamiyat ','Social Media'];
+
+// let textarray = subject.join('- ');
+//                                   ***  For joining two Array**///
+// let subject = ['Maths ', 'Physics', 'Chemistry', 'bio', 'English ', 'urdu ', 'Islamiyat ', 'Social Media'];
+// let subject2 = ['science', 'Alphabet'];
+// let newSubject = subject.concat(subject2);
+// console.log(newSubject)
+                                  //   *** for changing element,and index
+                                //    ***///
+// var a =['maths','18','science','BCA'];
+// console.log(a);
+//  a[0]="sunny";
+//  console.log(a);
+//  a[2]="Dilshan";
+//  console.log(a);
+//  a[3]="Fakhrii";
+//  console.log(a)
+
+// delete a[0];
+// console.log(a);                   //****  delete the element by using index value***
+
+
+// var a=['Zubair','Danish','GulBaz','Sher'];
+// console.log(a);
+// a.sort();
+// console.log(a);                  //*** place the element by using sort***//
+// a.reverse();
+// console.log(a);               //***this is called reverse sort */
+
+// var b= a.slice(1,3);
+// console.log(b);           /// *****    to remove the element****///
+
+                                    //  *** forEach Loop****///
+    //    var a=['Arslan','javeed','Gulbaz','jagar']
+    //    a.forEach(function(value){
+    //     console.log( value ); 
+    //    });
+    //                 //    ** object tutorial**///
+        var a = {
+            fname: 'Raza',
+            lname:  'Ullah',
+            age : 25,
+            email : 'rrixvi@gmal.com',
+            // favName:['athar','fakhar','gulbaz','shubnam'],        ///****We can make Array inside the object***/// */
+            // salary :function(){                                   ////****We can also make a function inside the object***/// */
+            //     return 25000;},
+        fulname : function(){
+        return this.fname + "" + this.lname;
+        
+        
+        }
+    }
+    
+           
+        console.log(a.fulname ());
+        document.write(a.fulname()); 
+        document.write(a.salary());
